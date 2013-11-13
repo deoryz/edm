f2f6f17157dd6c5df4647a27d2bf386af4936ebe
deory pandu <deoryzpandu@gmail.com>
2013-11-13 14:11:56 +0700

    edit pilihan score

--- protected/modules/admin/views/property/_form.php
+++ protected/modules/admin/views/property/_form.php
@@ -63,7 +63,27 @@
 
 	<?php echo $form->textFieldRow($model,'bind_satuan',array('class'=>'span3', 'placeholder'=>'ex: M (Milyar), Jt(Juta)')); ?>
 
-	<?php echo $form->textFieldRow($model,'score',array('class'=>'span3', 'placeholder'=>'0 - 100')); ?>
+	<?php echo $form->textFieldRow($model,'score', array(
+		'5'=>'5',
+		'10'=>'10',
+		'15'=>'15',
+		'20'=>'20',
+		'25'=>'25',
+		'30'=>'30',
+		'35'=>'35',
+		'40'=>'40',
+		'45'=>'45',
+		'50'=>'50',
+		'55'=>'55',
+		'60'=>'60',
+		'65'=>'65',
+		'70'=>'70',
+		'75'=>'75',
+		'80'=>'80',
+		'85'=>'85',
+		'90'=>'90',
+		'95'=>'95',
+	)); ?>
 
 	<?php echo $form->textAreaRow($model,'intro',array('rows'=>6, 'cols'=>50, 'class'=>'span8')); ?>
 
